504c01dee3957c5546b4a6c15473efc40417618e
Michael Westbom <totallymike@gmail.com>
2014-08-29 08:56:50 -0400

    Show issues in series

--- app/models/series.js
+++ app/models/series.js
@@ -2,12 +2,14 @@ import DS from 'ember-data';
 
 var Series = DS.Model.extend({
   title: DS.attr('string'),
-  volume: DS.attr('number')
+  volume: DS.attr('number'),
+
+  issues: DS.hasMany('issue', { async: true })
 })
 
 Series.reopenClass({
   FIXTURES: [
-    { id: 1, title: 'Action Comics', volume: 1 }
+    { id: 1, title: 'Action Comics', volume: 1, issues: [1, 2, 3] }
   ]
 })
 
