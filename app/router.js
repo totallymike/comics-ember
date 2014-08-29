b2c78b21b2b3617d2baa3ea6f12b5b5c8403eb3d
Michael Westbom <totallymike@gmail.com>
2014-08-29 12:52:45 -0400

    Move issues to their own route

--- app/router.js
+++ app/router.js
@@ -6,9 +6,8 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('series')
-  this.resource('series.show', { path: '/series/:series_id' }, function () {
-    this.resource('issue', { path: '/issue/:issue_id' })
-  })
+  this.resource('series.show', { path: '/series/:series_id' })
+  this.resource('issue', { path: '/issue/:issue_id' })
 });
 
 export default Router;
