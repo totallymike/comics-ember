1f4dc936cabd45bade6f20b5aec5823da6e362de
Michael Westbom <totallymike@gmail.com>
2014-09-04 14:10:36 -0400

    Think I figured out how to do uploads

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('bower_components/ember-uploader/dist/ember-uploader.js')
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
